e06009a00a0bc677841ad33a630f293daaa8b1cc
scotty <scotty107899@weexdev.com>
2026-06-07 11:18:39 +0800

    sh

--- src/save-header.ts
+++ src/save-header.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 
 async function main() {
-  console.log('save-header');
+  const headers = JSON.parse(JSON.stringify(
+    Object.fromEntries(
+      process.argv
+        .filter((_, index) => process.argv[index - 1] === '-H')
+        .map((value) => value.split(': '))
+    )
+  ));
+  console.log(headers);
 }
 
 main();
