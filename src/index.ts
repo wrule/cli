23207ff052c257eb636c941004f4ddc736a93ad8
scotty <scotty107899@weexdev.com>
2026-06-07 11:35:56 +0800

    req

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
+import axios from 'axios';
 
 async function main() {
-  console.log('req');
+  const method = process.argv[2]?.toUpperCase();
+  const url = process.argv[3];
+  const bodyJSON = process.argv[4];
+  axios.request({
+    method: 'POST',
+    url: '',
+  })
 }
 
 main();
