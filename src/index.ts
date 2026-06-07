dc3b4fc021e25b47ff0917106b4bdc70d05fdb55
scotty <scotty107899@weexdev.com>
2026-06-07 17:13:57 +0800

    scurl

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,18 @@ import axios from 'axios';
 
 async function main() {
   const fileName = 'common.json';
-  const headersDir = path.join(os.homedir(), '.headers', fileName);
+  const headersPath = path.join(os.homedir(), '.headers', fileName);
+  const headers = JSON.parse(fs.readFileSync(headersPath, 'utf8'));
+  const baseUrl = headers['x-req-baseurl'];
+  delete headers['x-req-baseurl'];
+
   const method = process.argv[2]?.toUpperCase()!;
-  const url = process.argv[3]!;
+  const url = path.join(baseUrl, process.argv[3]!);
   const body = process.argv[4];
   const res = await axios.request({
     method, url,
     data: body && JSON.parse(body),
-    headers: JSON.parse(fs.readFileSync(headersDir, 'utf8')),
+    headers,
   });
   console.log('-- Response Code --');
   console.log(res.status);
