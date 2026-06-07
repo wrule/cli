09ffc9aedea5edfb1ca1251120bc2a51b4df3bf0
scotty <scotty107899@weexdev.com>
2026-06-07 12:01:21 +0800

    req

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
 import axios from 'axios';
 
 async function main() {
-  const method = process.argv[2]?.toUpperCase();
-  const url = process.argv[3];
-  const bodyJSON = process.argv[4];
-  axios.request({
-    method: 'POST',
-    url: '',
-  })
+  const fileName = 'common.json';
+  const headersDir = path.join(os.homedir(), '.headers', fileName);
+  const method = process.argv[2]?.toUpperCase()!;
+  const url = process.argv[3]!;
+  const body = process.argv[4];
+  const res = await axios.request({
+    method, url,
+    params: body && JSON.parse(body),
+    headers: JSON.parse(fs.readFileSync(headersDir, 'utf8')),
+  });
+  console.log('-- Response Code --');
+  console.log(res.status);
+  console.log('-- Response Body --')
+  console.log(JSON.stringify(res.data, null, 2));
 }
 
 main();
